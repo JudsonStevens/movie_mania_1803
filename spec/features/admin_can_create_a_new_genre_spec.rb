690e31ecd43b1cb9f266f44baedd4ddf06a4c1bf
Judson Stevens <Judson.Stevens@gmail.com>
2018-06-05 10:43:53 -0600

    Update of genre creation test to get 100% coverage

--- spec/features/admin_can_create_a_new_genre_spec.rb
+++ spec/features/admin_can_create_a_new_genre_spec.rb
@@ -22,5 +22,25 @@ describe "User visits genre index page" do
       expect(page).to have_content(genre1.name)
       expect(page).to have_content(genre2.name)
     end
+    
+    it "is redirected back to genre index if creation doesn't work" do
+      admin = User.create(username: "Dee", password: "password", role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      genre1 = Genre.create(name: 'Sci-Fi')
+      genre2 = Genre.create(name: 'Horror')
+
+      name = 'Comedy'
+
+      visit(genres_path)
+
+      click_on('Create Genre')
+
+      expect(current_path).to eq(genres_path)
+      expect(page).to_not have_content(name)
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+    end
   end
 end
