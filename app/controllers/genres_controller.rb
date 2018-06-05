ee3f74a21987b0b01ea8eeb7c77041aee360a599
Judson Stevens <Judson.Stevens@gmail.com>
2018-06-05 09:21:09 -0600

    User story 3 done

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -3,4 +3,18 @@ class GenresController < ApplicationController
     @genres = Genre.all
     @genre = Genre.new
   end
+
+  def create
+    genre = Genre.new(genre_params)
+    if genre.save
+      redirect_to genres_path
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
---
+++ spec/features/admin_can_create_a_new_genre_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "User visits genre index page" do
+  context "as admin" do
+    it "can create a new genre" do
+      admin = User.create(username: "Dee", password: "password", role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      genre1 = Genre.create(name: 'Sci-Fi')
+      genre2 = Genre.create(name: 'Horror')
+
+      name = 'Comedy'
+
+      visit(genres_path)
+
+      fill_in('genre[name]', with: name)
+      click_on('Create Genre')
+
+      expect(current_path).to eq(genres_path)
+      expect(page).to have_content(name)
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+    end
+  end
+end
