c26437cb50882dda2da1ea421130a56e896e3109
Judson Stevens <Judson.Stevens@gmail.com>
2018-06-05 09:28:34 -0600

    Working on user story 4, WIP

---
+++ spec/features/visitor_cannot_see_genre_creation_form_but_can_see_all_genres_with_link_to_show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe 'Visitor' do
+  context 'visits the genre index page' do
+    it 'can see all genres with links to show pages, but cannot see form to create' do
+      genre1 = Genre.create(name: 'Sci-Fi')
+      genre2 = Genre.create(name: 'Horror')
+
+      visit(genres_path)
+
+      expect(page).to have_link(genre1.name)
+      expect(page).to have_link(genre2.name)
+    end
+
+    it 'can click on one of the links to go to that genres show page' do
+      genre1 = Genre.create(name: 'Sci-Fi')
+      genre2 = Genre.create(name: 'Horror')
+
+      visit(genres_path)
+      click_on(genre1.name)
+
+      expect(current_path).to eq(genre_path(genre1))
+      expect(page).to have_content(genre1.name)
+    end
+  end
+end
+
