491a89bc90f65db021c1236b74210ffdd9a2a869
Judson Stevens <Judson.Stevens@gmail.com>
2018-06-05 09:10:09 -0600

    User story 1 done

--- spec/features/admin_sees_genres_in_index_spec.rb
+++ spec/features/admin_sees_genres_in_index_spec.rb
@@ -8,7 +8,7 @@ describe "User visits genre index page" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
       genre1 = Genre.create(name: 'Sci-Fi')
-      genre2 = Gencre.create(name: 'Horror')
+      genre2 = Genre.create(name: 'Horror')
 
       visit(genres_path)
 
