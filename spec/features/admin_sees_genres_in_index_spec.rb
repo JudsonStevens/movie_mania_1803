e85d9bbb6b21cd08d5c7d3a80c71a2a1bf1edb7d
Judson Stevens <Judson.Stevens@gmail.com>
2018-06-05 09:07:39 -0600

    Working on user story 1, WIP

---
+++ spec/features/admin_sees_genres_in_index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "User visits genre index page" do
+  context "as admin" do
+    it "allows admin to see all genre" do
+      admin = User.create(username: "Dee", password: "password", role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+      genre1 = Genre.create(name: 'Sci-Fi')
+      genre2 = Gencre.create(name: 'Horror')
+
+      visit(genres_path)
+
+      expect(page).to have_content(genre1.name)
+      expect(page).to have_content(genre2.name)
+    end
+  end
+end
